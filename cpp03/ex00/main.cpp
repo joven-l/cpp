da7b5671b5d524e6bb80b7086dad3782f77d7bf5
joloo <joloo@student.42kl.edu.my>
2026-07-20 15:14:00 +0800

    refactor: test names feat: new tests

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -2,22 +2,30 @@
 #include <iostream>
 int main( void ) 
 {
-	ClapTrap a("a");
-	ClapTrap b("b");
-	ClapTrap c(a);
-	c = b;
+	ClapTrap c1("c1");
+	ClapTrap c2("c2");
+	ClapTrap c3(c1);
+	c3 = c1;
 
 	std::cout << std::endl;
-	a.attack("[thing]");
-	a.takeDamage(9);
+	c1.attack("[thing]");
+	c1.takeDamage(9);
 	for (int i = 0; i < 9; i++)
-		a.beRepaired(1);
+		c1.beRepaired(1);
 	std::cout << std::endl;
-	a.beRepaired(1);
-	a.attack("[thing]");
-	a.takeDamage(1000);
+	c1.beRepaired(1);
+	c1.attack("[thing]");
+	c1.takeDamage(1000);
 	std::cout << std::endl;
-	a.beRepaired(1);
-	a.attack("[thing]");
-	a.takeDamage(1000);
+	c1.beRepaired(1);
+	c1.attack("[thing]");
+	c1.takeDamage(1000);
+	std::cout << std::endl;
+	c2.beRepaired(1);
+	c2.attack("[thing]");
+	c2.takeDamage(1000);
+	std::cout << std::endl;
+	c3.beRepaired(1);
+	c3.attack("[thing]");
+	c3.takeDamage(1000);
 }
