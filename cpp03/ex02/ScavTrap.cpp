b92a4d704d853e9d73bf5d95b47ce325f7699e26
joloo <joloo@student.42kl.edu.my>
2026-07-20 15:28:46 +0800

    finish ex02

---
+++ cpp03/ex02/ScavTrap.cpp
@@ -0,0 +1,96 @@
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name), _name(name), _HP(scav_default_HP), _energy(scav_default_energy), _atk(scav_default_atk)
+{
+	std::cout << "Default Constructor called (ScavTrap " << this->_name << ")" << std::endl;
+	return ;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &param) : ClapTrap(param)
+{
+	this->_name = param._name;
+	this->_HP = param._HP;
+	this->_energy = param._energy;
+	this->_atk = param._atk;
+	std::cout << "Copy constructor called (ScavTrap " << this->_name << ")" << std::endl;
+	return ;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &param)
+{
+	ClapTrap::operator=(param);
+	this->_name = param._name;
+	this->_HP = param._HP;
+	this->_energy = param._energy;
+	this->_atk = param._atk;
+	std::cout << "Copy assignment operator called (ScavTrap " << this->_name << ")" << std::endl;
+	return (*this);
+}
+
+ScavTrap::~ScavTrap(void)
+{
+	std::cout << "Destructor called (ScavTrap " << this->_name << ")" << std::endl;
+	return ;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+	if (this->_HP == 0)
+	{
+		std::cout << "ScavTrap " << this->_name << " has no HP and cannot attack" << std::endl;
+		return ;
+	}
+	if (this->_energy == 0)
+	{
+		std::cout << "ScavTrap " << this->_name << " has no energy and cannot attack" << std::endl;
+		return ;
+	}
+	this->_energy--;
+	std::cout << "ScavTrap " << this->_name << " attacks " << target << " for " << this->_atk << " HP"
+	<< ". Remaining energy: " << this->_energy << std::endl;
+	return ;
+}
+
+void ScavTrap::takeDamage(unsigned int amount)
+{
+	if (this->_HP == 0)
+	{
+		std::cout << "ScavTrap " << this->_name << " already has 0 HP" << std::endl;
+		return ;
+	}
+	if (amount >= this->_HP)
+	{
+		std::cout << "ScavTrap " << this->_name << " takes " << this->_HP << " damage and dies" << std::endl;
+		this->_HP -= this->_HP;
+		return ;
+	}
+	this->_HP -= amount;
+	std::cout << "ScavTrap " << this->_name << " takes damage and loses " << amount << " HP"
+	<< ". Remaining HP: " << this->_HP << ". Remaining energy: " << this->_energy << std::endl;
+	return ;
+}
+
+void ScavTrap::beRepaired(unsigned int amount)
+{
+	if (this->_HP == 0)
+	{
+		std::cout << "ScavTrap " << this->_name << " has no HP and cannot heal" << std::endl;
+		return ;
+	}
+	if (this->_energy == 0)
+	{
+		std::cout << "ScavTrap " << this->_name << " has no energy and cannot heal" << std::endl;
+		return ;
+	}
+	this->_HP += amount;
+	this->_energy--;
+	std::cout << "ScavTrap " << this->_name << " is repaired for " << amount << " HP"
+	<< ". Remaining HP: " << this->_HP << ". Remaining energy: " << this->_energy << std::endl;
+	return ;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << this->_name << " is now in Gate keeper mode." << std::endl;
+}
---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,96 @@
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+#include <iostream>
+int main( void ) 
+{
+	ClapTrap c1("c1");
+	ClapTrap c2("c2");
+	ClapTrap c3(c1);
+	c3 = c1;
+
+	std::cout << std::endl;
+	c1.attack("[thing]");
+	c1.takeDamage(9);
+	for (int i = 0; i < 9; i++)
+		c1.beRepaired(1);
+	std::cout << std::endl;
+	c1.beRepaired(1);
+	c1.attack("[thing]");
+	c1.takeDamage(1000);
+	std::cout << std::endl;
+	c1.beRepaired(1);
+	c1.attack("[thing]");
+	c1.takeDamage(1000);
+	std::cout << std::endl;
+	c2.beRepaired(1);
+	c2.attack("[thing]");
+	c2.takeDamage(1000);
+	std::cout << std::endl;
+	c3.beRepaired(1);
+	c3.attack("[thing]");
+	c3.takeDamage(1000);
+	std::cout << std::endl;
+
+	ScavTrap s1("s1");
+	ScavTrap s2("s2");
+	ScavTrap s3(s1);
+	s3 = s2;
+
+	std::cout << std::endl;
+	s1.attack("[thing]");
+	s1.takeDamage(49);
+	for (int i = 0; i < 49; i++)
+		s1.beRepaired(1);
+	std::cout << std::endl;
+	s1.beRepaired(1);
+	s1.attack("[thing]");
+	s1.takeDamage(1000);
+	std::cout << std::endl;
+	s1.beRepaired(1);
+	s1.attack("[thing]");
+	s1.takeDamage(1000);
+	std::cout << std::endl;
+	s2.beRepaired(1);
+	s2.attack("[thing]");
+	s2.takeDamage(1000);
+	std::cout << std::endl;
+	s3.beRepaired(1);
+	s3.attack("[thing]");
+	s3.takeDamage(1000);
+	std::cout << std::endl;
+	s1.guardGate();
+	s2.guardGate();
+	s3.guardGate();
+	std::cout << std::endl;
+
+	FragTrap f1("f1");
+	FragTrap f2("f2");
+	FragTrap f3(f1);
+	f3 = f2;
+
+	std::cout << std::endl;
+	f1.attack("[thing]");
+	f1.takeDamage(99);
+	for (int i = 0; i < 99; i++)
+		f1.beRepaired(1);
+	std::cout << std::endl;
+	f1.beRepaired(1);
+	f1.attack("[thing]");
+	f1.takeDamage(1000);
+	std::cout << std::endl;
+	f1.beRepaired(1);
+	f1.attack("[thing]");
+	f1.takeDamage(1000);
+	std::cout << std::endl;
+	f2.beRepaired(1);
+	f2.attack("[thing]");
+	f2.takeDamage(1000);
+	std::cout << std::endl;
+	f3.beRepaired(1);
+	f3.attack("[thing]");
+	f3.takeDamage(1000);
+	std::cout << std::endl;
+	f1.highFivesGuys();
+	f2.highFivesGuys();
+	f3.highFivesGuys();
+}
