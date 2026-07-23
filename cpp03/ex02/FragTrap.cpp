f2889ad558b92159378857a1a1ab2bab70963ea8
joloo <joloo@student.42kl.edu.my>
2026-07-23 15:58:05 +0800

    fix: fix ex02 to reflect subject pdf requirements of inheritance

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -1,14 +1,22 @@
 #include "FragTrap.hpp"
 #include <iostream>
 
-FragTrap::FragTrap(void) : ClapTrap("Default"), _name("Default"), _HP(frag_default_HP), _energy(frag_default_energy), _atk(frag_default_atk)
+FragTrap::FragTrap(void) : ClapTrap("Default")
 {
+	this->_name = "Default";
+	this->_HP = frag_default_HP;
+	this->_energy = frag_default_energy;
+	this->_atk = frag_default_atk;
 	std::cout << "Default Constructor called (FragTrap " << this->_name << ")" << std::endl;
 	return ;
 }
 
-FragTrap::FragTrap(std::string name) : ClapTrap(name), _name(name), _HP(frag_default_HP), _energy(frag_default_energy), _atk(frag_default_atk)
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
 {
+	this->_name = name;
+	this->_HP = frag_default_HP;
+	this->_energy = frag_default_energy;
+	this->_atk = frag_default_atk;
 	std::cout << "Name constructor called (FragTrap " << this->_name << ")" << std::endl;
 	return ;
 }
@@ -37,62 +45,6 @@ FragTrap &FragTrap::operator=(const FragTrap &param)
 FragTrap::~FragTrap(void)
 {
 	std::cout << "Destructor called (FragTrap " << this->_name << ")" << std::endl;
-	return ;
-}
-
-void FragTrap::attack(const std::string& target)
-{
-	if (this->_HP == 0)
-	{
-		std::cout << "FragTrap " << this->_name << " has no HP and cannot attack" << std::endl;
-		return ;
-	}
-	if (this->_energy == 0)
-	{
-		std::cout << "FragTrap " << this->_name << " has no energy and cannot attack" << std::endl;
-		return ;
-	}
-	this->_energy--;
-	std::cout << "FragTrap " << this->_name << " attacks " << target << " for " << this->_atk << " HP"
-	<< ". Remaining energy: " << this->_energy << std::endl;
-	return ;
-}
-
-void FragTrap::takeDamage(unsigned int amount)
-{
-	if (this->_HP == 0)
-	{
-		std::cout << "FragTrap " << this->_name << " already has 0 HP" << std::endl;
-		return ;
-	}
-	if (amount >= this->_HP)
-	{
-		std::cout << "FragTrap " << this->_name << " takes " << this->_HP << " damage and dies" << std::endl;
-		this->_HP -= this->_HP;
-		return ;
-	}
-	this->_HP -= amount;
-	std::cout << "FragTrap " << this->_name << " takes damage and loses " << amount << " HP"
-	<< ". Remaining HP: " << this->_HP << ". Remaining energy: " << this->_energy << std::endl;
-	return ;
-}
-
-void FragTrap::beRepaired(unsigned int amount)
-{
-	if (this->_HP == 0)
-	{
-		std::cout << "FragTrap " << this->_name << " has no HP and cannot heal" << std::endl;
-		return ;
-	}
-	if (this->_energy == 0)
-	{
-		std::cout << "FragTrap " << this->_name << " has no energy and cannot heal" << std::endl;
-		return ;
-	}
-	this->_HP += amount;
-	this->_energy--;
-	std::cout << "FragTrap " << this->_name << " is repaired for " << amount << " HP"
-	<< ". Remaining HP: " << this->_HP << ". Remaining energy: " << this->_energy << std::endl;
 	return ;
 }
 
